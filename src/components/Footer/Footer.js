656ecf94d79598378dec3bed7582675c43140c72
Maximilian118 <maxcrosby118@gmail.com>
2021-06-23 16:50:00 +0200

    Github Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { GitHub } from '@material-ui/icons'
 
 const Footer = () => 
   <footer>
-    <h5>Maximilian Crosby &copy;</h5>
+    <h5>Maximilian Crosby</h5>
+    <Link to={{ pathname: "https://github.com/Maximilian118/Track-Runner-Frontend" }} target="_blank">
+      <GitHub/>
+    </Link>
   </footer>
 
 export default Footer
